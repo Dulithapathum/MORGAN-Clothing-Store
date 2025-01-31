c1d7cfdaecc37fdfbb62f2d7e2debccae6c946ad
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-31 15:39:18 +0530

    Create Pages And Create Routes

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Shop from "./Pages/Shop";
+import Contact from "./Pages/Contact";
+import ErrorPage from "./Pages/ErrorPage";
+import Cart from "./Pages/Cart";
+import CheckOut from "./Pages/CheckOut";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
+import SingleProduct from "./Pages/SingleProduct";
+import Logout from "./Pages/Logout";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<CheckOut />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/product/:id" element={<SingleProduct />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
